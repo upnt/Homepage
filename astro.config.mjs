ccfc4a6b3b162c12b95c4692c12f34228e90d8ec
upnt <upnt.github@gmail.com>
2025-07-23 01:14:11 +0900

    fix base dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://upnt.github.io",
-  base: "/public/",
+  base: "Homepage",
 
   vite: {
     plugins: [tailwindcss()],
