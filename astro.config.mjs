917382758fc2b5903a4bc2138959c819f32bef12
upnt <upnt.github@gmail.com>
2025-07-23 00:55:29 +0900

    fix jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://upnt.github.io",
+  base: "/public/",
 
   vite: {
     plugins: [tailwindcss()],
