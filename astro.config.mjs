b78040eb9a664b66a1d90dc25db01d3d7a4bf8b1
upnt <upnt.github@gmail.com>
2025-08-01 05:18:53 +0900

    add new pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +12,10 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
